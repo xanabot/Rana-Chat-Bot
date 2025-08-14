9dbc5d3f01f6ce7ca9af6bda237db0c75db778d0
RA NA <btor4085@gmail.com>
2025-08-14 19:52:56 +0400

    Update Owner.js

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "owner",
  version: "1.0.1",
  hasPermssion: 0,
- credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦",
+ credits: "RANA",
  description: "Owner information command with styled box",
  commandCategory: "Information",
  usages: "",
@@ -14,22 +14,22 @@ module.exports.run = async ({ api, event }) => {
 `╔═════════════════════╗
 ║ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 
 ╠═════════════════════╣
-║ 👤 𝗡𝗮𝗺𝗲 : 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓
-║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗔𝗛𝗨
-║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
+║ 👤 𝗡𝗮𝗺𝗲 :Abu Tahir 
+║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : RANA
+║ 🎂 𝗔𝗴𝗲 : 21+
 ║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
 ║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
 ║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗛𝗦𝗖
-║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗵𝗮𝗴𝗿𝗮𝗰𝗵𝗮𝗿𝗶
+║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : narayanganj
 ╠═════════════════════╣
 ║ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦 
 ╠═════════════════════╣
 ║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : 
-║ https://fb.com/100001039692046
+║ https://fb.com/100036047565461
 ║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 : 
 ║ m.me/100001039692046
 ║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 : 
-║ https://wa.me/01882333052
+║ https://wa.me/+971581156686
 ║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 : 
 ║ https://t.me/yoursahu
 ╚═════════════════════╝`;
